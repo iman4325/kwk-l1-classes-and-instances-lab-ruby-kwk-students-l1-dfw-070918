e997dacbc87a4791a7b259d914308b165c55b0ac
iman4325 <imantanzeem@hotmail.com>
2018-07-12 21:06:02 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,14 @@
 # person.rb
+# class Person 
+# def initialize(name)
+#   @name = name 
+# end
+# end 
+# adele_goldberg = Person.new("adele_goldberg")
+# alan_kay = Person.new("alan_kay")
+
 class Person 
- def initialize(name)
-   @name = name 
-end
+
 end 
-adele_goldberg = Person.new("adele_goldberg")
-alan_kay = Person.new("alan_kay")
+adele_goldberg = Person.new
+alan_kay = Person.new
